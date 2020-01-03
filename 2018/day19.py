3ffa40bcec0ccff13bd590878121a15ad14b4bfa
incnone <incohatus.none+git@gmail.com>
2020-01-02 19:44:12 -0500

    2018 day 19

--- 2018/day19.py
+++ 2018/day19.py
@@ -1,6 +1,16 @@
 from getinput import get_input
 import itertools
 import textwrap
+from program import Program, Decompile
+
+
+def decompile_input(s: str):
+    lines = s.splitlines(keepends=False)
+    ip = int(lines[0][-1])
+    print(lines)
+
+    decompiler = Decompile(ip_register=ip)
+    return '\n'.join(decompiler.decompile(idx, line) for idx, line in enumerate(lines[1:]))
 
 
 def parse_input(s: str):
@@ -8,21 +18,38 @@ def parse_input(s: str):
 
 
 def part_1(input_str: str):
-    return
+    # input_str = test_input()
+    prog = Program()
+    prog.init_from_str(input_str)
+    prog.execute()
+    return prog.reg[0]
+
+
+def div_sum(n):
+    return sum(x for x in range(1, n+1) if n % x == 0)
 
 
 def part_2(input_str: str):
-    return
+    # input_str = test_input()
+    # print(decompile_input(input_str))
+    return div_sum(10551389)
 
 
 def test_input():
     return textwrap.dedent("""\
-    """)
+    #ip 0
+    seti 5 0 1
+    seti 6 0 2
+    addi 0 1 0
+    addr 1 2 3
+    setr 1 0 0
+    seti 8 0 4
+    seti 9 0 5""")
 
 
 def main():
-    input_str = get_input()
-    print('Part 1:', part_1(input_str))
+    input_str = get_input(19)
+    # print('Part 1:', part_1(input_str))
     print('Part 2:', part_2(input_str))
 
 
--- 2018/program.py
+++ 2018/program.py
@@ -1,73 +1,274 @@
-
-def all_opcodes():
-    return [
-        Program.addr,
-        Program.addi,
-        Program.mulr,
-        Program.muli,
-        Program.banr,
-        Program.bani,
-        Program.borr,
-        Program.bori,
-        Program.setr,
-        Program.seti,
-        Program.gtir,
-        Program.gtri,
-        Program.gtrr,
-        Program.eqir,
-        Program.eqri,
-        Program.eqrr,
-    ]
+import string
 
 
 class Program(object):
-    def __init__(self):
-        self.reg = [0, 0, 0, 0]
+    @staticmethod
+    def all_opcodes():
+        return [
+            Program.addr,
+            Program.addi,
+            Program.mulr,
+            Program.muli,
+            Program.banr,
+            Program.bani,
+            Program.borr,
+            Program.bori,
+            Program.setr,
+            Program.seti,
+            Program.gtir,
+            Program.gtri,
+            Program.gtrr,
+            Program.eqir,
+            Program.eqri,
+            Program.eqrr,
+        ]
 
-    def addr(self, a, b, c):
+    @staticmethod
+    def get_opcode(s):
+        opcodes = {
+            'addr': Program.addr,
+            'addi': Program.addi,
+            'mulr': Program.mulr,
+            'muli': Program.muli,
+            'banr': Program.banr,
+            'bani': Program.bani,
+            'borr': Program.borr,
+            'bori': Program.bori,
+            'setr': Program.setr,
+            'seti': Program.seti,
+            'gtir': Program.gtir,
+            'gtri': Program.gtri,
+            'gtrr': Program.gtrr,
+            'eqir': Program.eqir,
+            'eqri': Program.eqri,
+            'eqrr': Program.eqrr,
+        }
+        return opcodes[s]
+
+    def __init__(self):
+        self.reg = [0, 0, 0, 0, 0, 0]
+        self.instr_ptr_idx = None
+        self.instrs = []
+
+    @property
+    def instr_ptr(self):
+        return self.reg[self.instr_ptr_idx]
+
+    def execute(self):
+        while 0 <= self.instr_ptr < len(self.instrs):
+            self.execute_next()
+
+    def execute_next(self):
+        opcode, a, b, c = self.instrs[self.instr_ptr]
+        opcode(s)
+        self.reg[self.instr_ptr_idx] += 1
+
+    def init_from_str(self, s):
+        self.reg = [0, 0, 0, 0, 0, 0]
+        self.instrs = []
+        lines = s.splitlines(keepends=False)
+        self.instr_ptr_idx = int(lines[0].split()[-1])
+        for line in lines[1:]:
+            words = line.split()
+            opcode = Program.get_opcode(words[0])
+            a, b, c = (int(x) for x in words[1:])
+            self.instrs.append((opcode, a, b, c))
+
+    def addr(s):
         self.reg[c] = self.reg[a] + self.reg[b]
 
-    def addi(self, a, b, c):
+    def addi(s):
         self.reg[c] = self.reg[a] + b
 
-    def mulr(self, a, b, c):
+    def mulr(s):
         self.reg[c] = self.reg[a] * self.reg[b]
 
-    def muli(self, a, b, c):
+    def muli(s):
         self.reg[c] = self.reg[a] * b
 
-    def banr(self, a, b, c):
+    def banr(s):
         self.reg[c] = self.reg[a] & self.reg[b]
 
-    def bani(self, a, b, c):
+    def bani(s):
         self.reg[c] = self.reg[a] & b
 
-    def borr(self, a, b, c):
+    def borr(s):
         self.reg[c] = self.reg[a] | self.reg[b]
 
-    def bori(self, a, b, c):
+    def bori(s):
         self.reg[c] = self.reg[a] | b
 
-    def setr(self, a, b, c):
+    def setr(s):
         self.reg[c] = self.reg[a]
 
-    def seti(self, a, b, c):
+    def seti(s):
         self.reg[c] = a
 
-    def gtir(self, a, b, c):
+    def gtir(s):
         self.reg[c] = 1 if a > self.reg[b] else 0
 
-    def gtri(self, a, b, c):
+    def gtri(s):
         self.reg[c] = 1 if self.reg[a] > b else 0
 
-    def gtrr(self, a, b, c):
+    def gtrr(s):
         self.reg[c] = 1 if self.reg[a] > self.reg[b] else 0
 
-    def eqir(self, a, b, c):
+    def eqir(s):
         self.reg[c] = 1 if a == self.reg[b] else 0
 
-    def eqri(self, a, b, c):
+    def eqri(s):
         self.reg[c] = 1 if self.reg[a] == b else 0
 
-    def eqrr(self, a, b, c):
+    def eqrr(s):
         self.reg[c] = 1 if self.reg[a] == self.reg[b] else 0
+
+
+class Decompile(object):
+    @staticmethod
+    def get_opcode(s):
+        opcodes = {
+            'addr': Decompile.addr,
+            'addi': Decompile.addi,
+            'mulr': Decompile.mulr,
+            'muli': Decompile.muli,
+            'banr': Decompile.banr,
+            'bani': Decompile.bani,
+            'borr': Decompile.borr,
+            'bori': Decompile.bori,
+            'setr': Decompile.setr,
+            'seti': Decompile.seti,
+            'gtir': Decompile.gtir,
+            'gtri': Decompile.gtri,
+            'gtrr': Decompile.gtrr,
+            'eqir': Decompile.eqir,
+            'eqri': Decompile.eqri,
+            'eqrr': Decompile.eqrr,
+        }
+        return opcodes[s]
+    
+    def __init__(self, ip_register):
+        self.ipr = ip_register
+        self.idx = 0
+
+    def reg(self, n):
+        if n == self.ipr:
+            return self.idx
+        else:
+            return string.ascii_lowercase[n]
+
+    def decompile(self, idx, s):
+        self.idx = idx
+        words = s.split()
+        opcode = Decompile.get_opcode(words[0])
+        a, b, c = (int(x) for x in words[1:])
+        return '{:>3}: {}'.format(idx, opcode(self, a, b, c))
+    
+    def addr(self, a, b, c):
+        if c == self.ipr:
+            if a == self.ipr:
+                return 'jump {}+1'.format(self.reg(b))
+            elif b == self.ipr:
+                return 'jump {}+1'.format(self.reg(a))
+            else:
+                return 'goto {}+{}+1'.format(self.reg(a), self.reg(b))
+        else:
+            if c == a:
+                return '{} += {}'.format(self.reg(c), self.reg(b))
+            elif c == b:
+                return '{} += {}'.format(self.reg(c), self.reg(a))
+            else:
+                return '{} = {}+{}'.format(self.reg(c), self.reg(a), self.reg(b))
+
+    def addi(self, a, b, c):
+        if c == self.ipr:
+            if a == self.ipr:
+                return 'jump {}'.format(b+1)
+            else:
+                return 'goto {}+{}'.format(self.reg(a), b+1)
+        else:
+            if c == a:
+                return '{} += {}'.format(self.reg(c), b)
+            elif c == b:
+                return '{} += {}'.format(self.reg(c), self.reg(a))
+            else:
+                return '{} = {}+{}'.format(self.reg(c), self.reg(a), b)
+
+    def mulr(self, a, b, c):
+        if c == self.ipr:
+            if a == self.ipr:
+                return 'wtf {}'.format(self.reg(b))
+            elif b == self.ipr:
+                return 'wtf {}'.format(self.reg(a))
+            else:
+                return 'goto {}*{}+1'.format(self.reg(a), self.reg(b))
+        else:
+            if c == a:
+                return '{} *= {}'.format(self.reg(c), self.reg(b))
+            elif c == b:
+                return '{} *= {}'.format(self.reg(c), self.reg(a))
+            else:
+                return '{} = {}*{}'.format(self.reg(c), self.reg(a), self.reg(b))
+
+    def muli(self, a, b, c):
+        if c == self.ipr:
+            if a == self.ipr:
+                return 'wtf {}'.format(b)
+            else:
+                return 'goto {}*{}+1'.format(self.reg(a), b)
+        else:
+            if c == a:
+                return '{} *= {}'.format(self.reg(c), b)
+            elif c == b:
+                return '{} *= {}'.format(self.reg(c), self.reg(a))
+            else:
+                return '{} = {}*{}'.format(self.reg(c), self.reg(a), b)
+
+    def banr(self, a, b, c):
+        return 'banr NOT IMPLEMENTED IN DECOMPILER'
+
+    def bani(self, a, b, c):
+        return 'bani NOT IMPLEMENTED IN DECOMPILER'
+    
+    def borr(self, a, b, c):
+        return 'borr NOT IMPLEMENTED IN DECOMPILER'
+
+    def bori(self, a, b, c):
+        return 'bori NOT IMPLEMENTED IN DECOMPILER'
+    
+    def setr(self, a, b, c):
+        if c == a:
+            return 'nop'
+        elif c == self.ipr:
+            return 'goto {}+1'.format(self.reg(a))
+        else:
+            return '{} = {}'.format(self.reg(c), self.reg(a))
+    
+    def seti(self, a, b, c):
+        if c == self.ipr:
+            return 'goto {}'.format(a+1)
+        else:
+            return '{} = {}'.format(self.reg(c), a)
+    
+    def gtir(self, a, b, c):
+        return 'gtir NOT IMPLEMENTED IN DECOMPILER'
+    
+    def gtri(self, a, b, c):
+        return 'gtri NOT IMPLEMENTED IN DECOMPILER'
+    
+    def gtrr(self, a, b, c):
+        if c == self.ipr:
+            return 'gtrr(ip) NOT IMPLEMENTED IN DECOMPILER'
+        else:
+            return '{} = ({} < {})'.format(self.reg(c), self.reg(b), self.reg(a))
+    
+    def eqir(self, a, b, c):
+        return 'eqir NOT IMPLEMENTED IN DECOMPILER'
+    
+    def eqri(self, a, b, c):
+        return 'eqri NOT IMPLEMENTED IN DECOMPILER'
+    
+    def eqrr(self, a, b, c):
+        if c == self.ipr:
+            return 'gtrr(ip) NOT IMPLEMENTED IN DECOMPILER'
+        else:
+            return '{} = ({} == {})'.format(self.reg(c), self.reg(b), self.reg(a))
